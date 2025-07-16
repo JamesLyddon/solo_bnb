aba44d329a00d90a978d1a651a9cd55569e9a178
james lyddon <jameslyddon@gmail.com>
2025-07-16 13:35:09 +0100

    finished listing model

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, render_template, redirect, url_for
 from lib.database_connection import get_flask_database_connection
 from lib.models.user import User
 from lib.repos.user_repo import UserRepo
+from lib.models.listing import Listing
+from lib.models.listing_repo import ListingRepo
 
 
 # Create a new Flask app
